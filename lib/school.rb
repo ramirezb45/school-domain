0efe62bc1c17a5ad22db4943324610328bb314d5
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-11 19:21:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
-# code here!
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.roster
+
+hash["new_key"] = []
+hash["new_key"] << "new_value_for_value_array"
+ 
+ hash
+ 
